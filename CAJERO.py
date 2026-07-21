a8808009e65089e24bd9a897f1560401a9ac0e87
any94-create <anaslopez48@gmail.com>
2026-07-21 13:28:42 -0600

    Refactor ATM functions and variable names

--- CAJERO.py
+++ CAJERO.py
@@ -18,11 +18,11 @@ def menu():
   choice = input("Por favor, seleccione una opción (1-4): ")
 
   # devuelve la opción elegida por el usuario a quien realiza la llamada.
-  return choice 
+  return opcion
 
   # FUNCION PARA CONSULTAR EL SALDO
 
-  def check_balance(): 
+  def consulta_saldo(): 
 
     # Esto recupera el saldo del diccionario de la cuenta y lo imprime en una cadena formateada.
     print(f"Su saldo actual es: ${cuenta["saldo"]}") 
@@ -62,3 +62,29 @@ def menu():
           print("Cantidad no válida. Introduzca un número positivo.")
 
  # Función principal para hacer funcionar el cajero automático
+
+def run_atm(): 
+    # Solicitar al usuario que ingrese el PIN 
+    pin = input("Por favor, ingrese su PIN: ") 
+    
+    # Comprobar si el PIN ingresado es correcto 
+    if pin == cuenta["pin"]:
+        #  Esto inicia un bucle infinito para mantener el cajero automático en funcionamiento hasta que el usuario decida salir.
+        while True: 
+            # llma al menu para mostrar el menú y obtener la elección del usuario.
+            opcion = menu() 
+            if opcion == '1': 
+                consulta_saldo() 
+            elif opcion == '2': 
+                depositar_dinero() 
+            elif opcion == '3': 
+                retirar_dinero() 
+            elif opcion == '4': 
+                print("Gracias por usar el cajero automático. ¡Adiós!") 
+                break 
+            else: 
+                print("Opción no válida. Por favor, seleccione una opción válida (1-4).") 
+    else: 
+        print("PIN incorrecto. Por favor, inténtelo de nuevo.") 
+
+ejecutar_cajero automático() 
